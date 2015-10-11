6afc3e5c087c0e1d3758be94c1ab02c7b76fd185
turtlefeng <turtlefeng@gmail.com>
2015-10-11 14:35:40 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
+window.comfirm("歡迎來到馮翼的履歷");
 if (comfirm("歡迎來到馮翼的履歷") == true) {
-    window.alert("請慢慢參觀\n有需要可以在下方連結跟我聯絡\n <3 <3 <3");
+    window.alert("請慢慢參觀\n有需要可以在下方連結跟我聯絡");
 } 
 else {
     window.alert("下次記得去對網頁喔~~");
