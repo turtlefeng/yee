11833a5d3e5b034474d054bd65064afb6ce959a7
turtlefeng <turtlefeng@gmail.com>
2015-10-25 17:17:22 +0800

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,3 +1,8 @@
-#!/usr/local/bin/node
-console.log("Content-type: text/html\n\n");
-console.log("<h1>Hello World!</h1>");
+var http = require('http');
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    response.end('Hello World\n');
+}).listen(8000);
+
+console.log('Server running at http://127.0.0.1:8000/');
