5e4b2ee8e98b03b6a245b1bc4e0c3cd7a3caad70
turtlefeng <turtlefeng@gmail.com>
2015-10-27 01:09:56 +0800

    Update page1.js

--- page1.js
+++ page1.js
@@ -1,42 +1,61 @@
-http = require 'http'  
-url = require 'url'  
-qs = require 'querystring'  
-fs = require 'fs'  
+function() {  
+  var fs, http, qs, server, url;  
   
-server = http.createServer (req,res)->  
-   urlData = url.parse req.url,true  
-   action = urlData.pathname  
-   publicPath = __dirname + "\\public\\"  
+  http = require('http');  
   
-   if action is "/Signup"  
-      if req.method is "POST"  
-       formData = ''  
-       msg = ''  
-       req.on 'data',(data)->  
-         formData += data  
-           
-         req.on 'end', ->  
-           user = qs.parse formData             
-           user.id = "123456"  
-           msg = JSON.stringify user  
-           res.writeHead 200,{"Content-Type":"application/json","Content-Length":msg.length}  
-           res.end msg  
-           
-      else  
-       form = publicPath + "ajaxSignupForm.html"  
-       fs.readFile form,(err,contents) ->  
-         #檔案如果沒發生錯誤，就把讀取進來的內容送出去，否則送出500這個錯誤狀態碼  
-         if err isnt true  
-           res.writeHead 200,"Content-Type":"text/html"  
-           res.end(contents)  
-         else  
-           res.writeHead 500  
-           res.end  
+  url = require('url');  
   
-   else  
-      res.writeHead 200,"Content-Type":"text/html"  
-      res.end "<h1>歡迎光臨Node.js菜鳥筆記</h1><p><a href=\"/Signup\">註冊</a></p>"  
-        
-server.listen 3000  
+  qs = require('querystring');  
   
-console.log "Server跑起來了，現在時間是" + new Date() 
+  fs = require('fs');  
+  
+  server = http.createServer(function(req, res) {  
+    var action, form, formData, msg, publicPath, urlData;  
+    urlData = url.parse(req.url, true);  
+    action = urlData.pathname;  
+    publicPath = __dirname + "\\public\\";  
+    if (action === "/Signup") {  
+      if (req.method === "POST") {  
+        formData = '';  
+        msg = '';  
+        return req.on('data', function(data) {  
+          formData += data;  
+          return req.on('end', function() {  
+            var user;  
+            user = qs.parse(formData);  
+            user.id = "123456";  
+            msg = JSON.stringify(user);  
+            res.writeHead(200, {  
+              "Content-Type": "application/json",  
+              "Content-Length": msg.length  
+            });  
+            return res.end(msg);  
+          });  
+        });  
+      } else {  
+        form = publicPath + "ajaxSignupForm.html";  
+        return fs.readFile(form, function(err, contents) {  
+          if (err !== true) {  
+            res.writeHead(200, {  
+              "Content-Type": "text/html"  
+            });  
+            return res.end(contents);  
+          } else {  
+            res.writeHead(500);  
+            return res.end;  
+          }  
+        });  
+      }  
+    } else {  
+      res.writeHead(200, {  
+        "Content-Type": "text/html"  
+      });  
+      return res.end("<h1>歡迎光臨Node.js菜鳥筆記</h1><p><a href=\"/Signup\">註冊</a></p>");  
+    }  
+  });  
+  
+  server.listen(3000);  
+  
+  console.log("Server跑起來了，現在時間是" + new Date());  
+  
+}).call(this);
